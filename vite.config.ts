4cb0fd926b13955f6103fb9dde7a07d0744aa732
gowaganesh <gowaganesh27@gmail.com>
2024-11-11 21:45:49 +0530

    vite config files updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : "/Portfolio",
   assetsInclude: ['**/*.md'],
   resolve: {
     alias: {
